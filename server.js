dfa6747f1c7f6c8e2cf5cb9fd17611f62df523c2
Adam Baker <albcodingbootcamp@gmail.com>
2020-02-19 19:21:06 -0500

    added heroku db

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 // Define API routes here
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3-react");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds229118.mlab.com:29118/heroku_7hg6fxv2");
 
 // Send every other request to the React app
 // Define any API routes before this runs
